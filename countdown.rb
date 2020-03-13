7da30842bc41e9e5730f4391dc58c96bf836a0c9
Shaina C <scailles23@gmail.com>
2020-03-13 22:41:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,7 @@ def countdown(number= 10)
   while number>0
     puts "#{number} SECOND(S)!"
     number -= 1
-  end
-  until number==0
-    return "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
   end
     
 
