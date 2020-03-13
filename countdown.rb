d4aa35991e05b95b6dcce189dc2bd684a9fe450d
Shaina C <scailles23@gmail.com>
2020-03-13 22:47:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,7 @@ def countdown(number= 10)
   "HAPPY NEW YEAR!"
 end
     
-countdown_with_sleep(number=10)
+def countdown_with_sleep(number=10)
 while number > 0
   puts "#{number} SECOND(S)!"
     number -= 1
